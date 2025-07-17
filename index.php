53f704d597f0336c59b61b416a01e12a3714be63
Fraxoo <fraxo.officiel@gmail.com>
2025-07-17 10:18:43 +0200

    site fonctionel

--- index.php
+++ index.php
@@ -2,35 +2,27 @@
 
 session_start();
 
-echo $_SESSION['id'];
-echo $_SESSION['nom'];
+
 
 $user = "root";
 $pass = "root";
 
-try
-
-{
-$bdd = new PDO('mysql:host=127.0.0.1;port=3306;dbname=app-database', $user, $pass);
-}
-
-catch (Exception $e)
-
-{
+try {
+    $bdd = new PDO('mysql:host=127.0.0.1;port=3306;dbname=app-database', $user, $pass);
+} catch (Exception $e) {
 
     die('Erreur : ' . $e->getMessage());
-
 }
 
 $addtask = $_POST["addtask"];
 
-if(isset($addtask)){
+if (isset($addtask)) {
 
-$insertask = $bdd->prepare('INSERT INTO task(task,acount_id) VALUES (:task,:acount_id)');
-$insertask->execute([
-    'task' => $addtask,
-    'acount_id' => $_SESSION['id']
-]);
+    $insertask = $bdd->prepare('INSERT INTO task(task,acount_id) VALUES (:task,:acount_id)');
+    $insertask->execute([
+        'task' => $addtask,
+        'acount_id' => $_SESSION['id']
+    ]);
 }
 
 $tasks = $bdd->query('SELECT * FROM task');
@@ -38,10 +30,13 @@ $tasks->execute();
 $taches = $tasks->fetchAll();
 
 
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -56,45 +51,59 @@ $taches = $tasks->fetchAll();
 
 <body>
 
-<?php if(!isset($_SESSION['id'])):?>
-<div class="connect">
-    <a href="inscription.php">S'inscrire</a>
-    <a href="connexion.php">Se connecter</a>
-</div>
-<?php else :?>
-<div class="utilisateur">
-    <p>Bonjour
-</div>
-
-<?php endif ?>
-
-<div class="main">
-    <div class="matodo">
-        <p>Ma</p>
-        <h1>To do list</h1>
-    </div>
-
-    <div class="add">
-        <form action="/" method="post">
-            <input type="text" name="addtask" placeholder="Ajoutez une tache :" required>
-            <button type="submit">Ajoutez</button>
-        </form>
-    </div>
-
-    <?php foreach($taches as $tache):?>
-
-    <div class="taches">
-
-        <div class="list">
-            <input type="checkbox">
-            <?php echo $tache['task']."\n"?>
+    <?php if (!isset($_SESSION['id'])): ?>
+        <div class="connect">
+            <a href="inscription.php">S'inscrire</a>
+            <a href="connexion.php">Se connecter</a>
         </div>
+    <?php else : ?>
+
         
-        <?php endforeach;?>
+            
+            <div class="utilisateur">
+                <p>Bonjour <?= $_SESSION['nom']; ?></p>
+            </div>
+            <div class="deco">
+                <a href="deconnexion.php">Se Deconnecter</a>
+            </div>
+        
+    <?php endif ?>
+
+    <div class="main">
+        <div class="matodo">
+            <p>Ma</p>
+            <h1>To do list</h1>
+        </div>
+
+        <div class="add">
+            <form action="/" method="post">
+                <input type="text" name="addtask" placeholder="Ajoutez une tache :" required>
+                <button type="submit">Ajoutez</button>
+            </form>
+        </div>
+
+        <?php foreach ($taches as $tache): ?>
+
+            <?php if ($_SESSION['id'] == $tache['acount_id']) : ?>
+
+                <div class="taches">
+
+                    <div class="list">
+                        <input type="checkbox">
+                        <?php echo $tache['task'] . "\n" ?>
+                    </div>
+
+                <?php endif ?>
+
+            <?php endforeach; ?>
+
+                </div>
+            
 
     </div>
 
-</div>
 
+   
 </body>
+
 </html>
